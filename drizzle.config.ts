d1ba84a8fd1aa745c0b5d57d43af95b74469b847
Haikhal Fakhreez <haikhalfakhreez@gmail.com>
2023-10-22 16:57:19 +0800

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,14 @@ import type { Config } from "drizzle-kit"
 export default {
   schema: "./server/db/schema.ts",
   out: "./server/db/migrations",
-  driver: "better-sqlite",
-  dbCredentials: {
-    url: "./server/db/sqlite.db",
-  },
+  ...(process.env.DB
+    ? {
+        breakpoints: true,
+      }
+    : {
+        driver: "better-sqlite",
+        dbCredentials: {
+          url: "./server/db/sqlite.db",
+        },
+      }),
 } satisfies Config
