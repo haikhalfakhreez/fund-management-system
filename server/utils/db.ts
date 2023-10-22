7bb72acfbc8476554feb3b34146e0db943463481
Haikhal Fakhreez <haikhalfakhreez@gmail.com>
2023-10-22 17:31:08 +0800

    update useDb

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,20 +1,18 @@
 import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1"
 import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3"
 import Database from "better-sqlite3"
-import * as schema from "~/server/db/schema"
 
-let _db:
-  | DrizzleD1Database<typeof schema>
-  | BetterSQLite3Database<typeof schema>
-  | null = null
+let _db: DrizzleD1Database | BetterSQLite3Database | null = null
 
 export const useDb = () => {
   if (!_db) {
     if (process.env.DB) {
-      _db = drizzleD1(process.env.DB, { schema })
-    } else {
+      _db = drizzleD1(process.env.DB)
+    } else if (process.dev) {
       const sqlite = new Database("server/db/sqlite.db")
-      _db = drizzle(sqlite, { schema })
+      _db = drizzle(sqlite)
+    } else {
+      throw new Error("No database configured for production")
     }
   }
 
