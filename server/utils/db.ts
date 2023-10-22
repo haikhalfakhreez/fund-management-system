fa80e55ac816a56cfed69fd9f308e300b2a0332f
Haikhal Fakhreez <haikhalfakhreez@gmail.com>
2023-10-22 16:10:43 +0800

    update db utils to include cloudflare d1

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,13 +1,22 @@
+import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1"
 import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3"
 import Database from "better-sqlite3"
 import * as schema from "~/server/db/schema"
 
-let _db: BetterSQLite3Database<typeof schema> | null = null
+let _db:
+  | DrizzleD1Database<typeof schema>
+  | BetterSQLite3Database<typeof schema>
+  | null = null
 
 export const useDb = () => {
   if (!_db) {
-    const sqlite = new Database("server/db/sqlite.db")
-    _db = drizzle(sqlite, { schema })
+    if (process.env.DB) {
+      _db = drizzleD1(process.env.DB, { schema })
+    } else {
+      const sqlite = new Database("server/db/sqlite.db")
+      _db = drizzle(sqlite, { schema })
+    }
   }
+
   return _db
 }
